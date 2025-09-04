171ac0fb81c2c4f0e27059a70000b917d00fb46a
SangHyun-e <66759329+SangHyun-e@users.noreply.github.com>
2025-09-04 21:12:56 +0900

    fix: add @Param(Method Parameter Naming)  in CommentRepository

--- src/main/java/io/routepickapi/config/QuerydslConfig.java
+++ src/main/java/io/routepickapi/config/QuerydslConfig.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /*
-* QueryDSL에서 JPQL 작성/실행을 도와주는 JPAQueryFactory Bean 등록
-* - EntityManager를 주입받아 재사용함(스레드 세이프 관점에서 생성자 주입이 더 유리함)
-*/
+ * QueryDSL 에서 JPQL 작성/실행을 도와주는 JPAQueryFactory Bean 등록
+ * - EntityManager 를 주입받아 재사용함(스레드 세이프 관점에서 생성자 주입이 더 유리함)
+ */
 @Configuration
 public class QuerydslConfig {
 
